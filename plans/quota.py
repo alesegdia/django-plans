444e84da3490db8860cf63d628041f745ac179db
Stéphane K <carxwol@hexecho.net>
2015-03-12 13:06:39 +0100

    Fix get_user_quota with multiplans

--- plans/quota.py
+++ plans/quota.py
@@ -2,5 +2,8 @@ def get_user_quota(user):
     """
     Tiny helper for getting quota dict for user (left mostly for backward compatibility)
     """
-    return user.userplan.plan.get_quota_dict()
+    userplans = {}
+    for userplan in user.userplan_set.iterator():
+        userplans[userplan] = userplan.plan.get_quota_dict()
+    return userplans
 
